796fbd262567baa16b2d0bd09d23d8082d9bc3a1
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-08-24 19:08:26 +0300

    multiomics prostate queyr

--- compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
+++ compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
@@ -5,7 +5,7 @@ import framework.common._
 import framework.examples.Query
 import framework.nrc.Parser
 
-object MultiOmicsProstate extends DriverGene {
+object MultiOmicsProstateFinal extends DriverGene {
   val sampleFile = "/mnt/app_hdd/data/biospecimen/aliquot/nationwidechildrens.org_biospecimen_aliquot_prad.txt"
   val cnvFile = "/mnt/app_hdd/data/cnv"
   val exprFile = "/mnt/app_hdd/data/expression/"
@@ -27,7 +27,7 @@ object MultiOmicsProstate extends DriverGene {
         |""".stripMargin
 
   // name to identify your query
-  val name = "ExampleQueryMultiOmicsProstate"
+  val name = "MultiOmicsProstateFinal"
 
   // a map of input types for the parser
     val tbls = Map("occurrences" -> occurmids.tp,
