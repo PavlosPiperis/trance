45b9f6bb63a043ae9f27fc7fe95ec91f476fd3aa
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-09-14 18:51:36 +0300

    fix?

--- compiler/src/main/scala/framework/generator/spark/App.scala
+++ compiler/src/main/scala/framework/generator/spark/App.scala
@@ -152,7 +152,7 @@ object App {
   val schema = TPCHSchema.getSchema()
 
   def main(args: Array[String]){
-    AppWriter.runDatasetShred(MultiOmicsProstateFinal, "bugfix", optLevel = 2, schema = schema, unshred=true)
+    AppWriter.runDatasetShred(MultiOmicsProstateFinalRuntimes, "bugfix", optLevel = 2, schema = schema, unshred=true)
     // runFlatToNested()
     // runNestedToNested()
     // runNestedToFlat()
